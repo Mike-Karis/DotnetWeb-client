21703d43cc7aff91cb7a6d75d62ff5e57d093193
Michael karis <mkaris1@my.wctc.edu>
2022-02-03 09:40:58 -0600

    randomized happy birthday page load

--- js/balloons.js
+++ js/balloons.js
@@ -1,6 +1,9 @@
 $(function(){
     $('#birthday').pickadate({ format: 'mmmm, d' });
 
+    const animations=["animate__bounce","animate__flash","animate__pulse","animate__rubberBand","animate__shakeX","animate__shakeY","animate__headShake","animate__swing","animate__tada","animate__wobble","animate__jello","animate__heartBeat"]
+    var rand=Math.floor(Math.random() * 12);
+    $('h1').removeClass().addClass('animate__animated '+animations[rand]);
     // uncheck all checkboxes (FireFox)
     $('.form-check-input').each(function () {
         $(this).prop('checked', false);
